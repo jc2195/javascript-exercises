6ee1afd14e4be6402d319f9448d3dea6a02f548c
Kuba <k.ciukiewicz@gmail.com>
2020-07-30 21:54:18 +0100

    Completed repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0){
+        return 'ERROR';
+    }
+    else {
+        let result = "";
+        for (i=num; i>0; i--) {
+            result = result + string;
+        }
+        return result;
+    }
 }
 
 module.exports = repeatString
