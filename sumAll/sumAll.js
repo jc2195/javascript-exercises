0b6f5c41583e5afbf91735eee6068665f04d4bc6
Kuba <k.ciukiewicz@gmail.com>
2020-07-31 03:19:30 +0100

    Completed sumAll.js

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (((num1 >= 0) && (num2 >= 0)) && ((typeof(num1) == 'number') && (typeof(num2) == 'number'))) {
+        let smallerNumber = 0;
+        let biggerNumber = 0;
+        let sum = 0;
+        if (num1 > num2) {
+            smallerNumber = num2;
+            biggerNumber = num1;
+        }
+        else {
+            smallerNumber = num1;
+            biggerNumber = num2;
+        }
+        for (i = smallerNumber; i <= biggerNumber; i++) {
+            sum += i
+        }
+        return sum;
+    }
+    else {
+        return "ERROR";
+    }
 }
 
 module.exports = sumAll
