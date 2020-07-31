3644c3fa4052cebdcce70d022141f58b1bff5f1e
Kuba <k.ciukiewicz@gmail.com>
2020-07-31 02:52:13 +0100

    Completed removeFromArray.js

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    for (i = 1; i <= arguments.length; i++) {
+        let position = array.indexOf(arguments[i]);
+        while (position != -1) {
+            array.splice(position, 1);
+            position = array.indexOf(arguments[i]);
+        }
+    }
+    return array;
 }
 
 module.exports = removeFromArray
