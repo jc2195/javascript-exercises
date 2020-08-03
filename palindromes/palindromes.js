f4f1bd7907439a6f704e3838ec123b4aac2e2cb0
Kuba <k.ciukiewicz@gmail.com>
2020-08-03 14:42:14 +0100

    Completed palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    string = string.toLowerCase();
+    string = string.split("");
+    string = string.filter(index => index >= "a" && index <= "z");
+    let normalLetters = string.join("");
+    let reversedLetters = string.slice().reverse().join("");
+    return normalLetters == reversedLetters;
 }
 
 module.exports = palindromes
