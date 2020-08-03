67b7f84d7cbf35219cad9097a9a61a2844fde8b8
Kuba <k.ciukiewicz@gmail.com>
2020-08-03 17:22:37 +0100

    Completed findTheOldest.js

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-let findTheOldest = function() {
-
-}
+let findTheOldest = function(people) {
+    const oldest = people.sort((a, b) => {
+        let lastGuy = 0;
+        let nextGuy = 0;
+        const myDate = new Date();
+        if ("yearOfDeath" in a) {
+            lastGuy = a.yearOfDeath - a.yearOfBirth;
+        }
+        else {
+            lastGuy = myDate.getFullYear() - a.yearOfBirth;
+        }
+        if ("yearOfDeath" in b) {
+            nextGuy = b.yearOfDeath - b.yearOfBirth;
+        }
+        else {
+            nextGuy = myDate.getFullYear() - b.yearOfBirth;
+        }
+        return lastGuy > nextGuy ? -1: 1;
+    });
+    return oldest[0];
+} 
 
 module.exports = findTheOldest
