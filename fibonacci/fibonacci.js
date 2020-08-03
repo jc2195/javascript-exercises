4beee19bd0c417a961a9837159402a1c256657c6
Kuba <k.ciukiewicz@gmail.com>
2020-08-03 16:50:51 +0100

    Completed fibonacci.js

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
-}
+const fibonacci = function(a) {
+    if (a > 0) {
+        let fibArray = [1, 1];
+        if (a <= 2){
+            return fibArray[fibArray.length - 1];
+        }
+        else {
+            for (i = 3; i <= a; i++){
+                let newElement = fibArray[i-2] + fibArray[i-3];
+                fibArray.push(newElement);
+            }
+        }
+        return fibArray[fibArray.length - 1];
+    }
+    else {
+        return "OOPS";
+    }
+} 
 
 module.exports = fibonacci
